8543ae6dd0e124c3181213eeb2980b6ce38d6c8a
Gladys Maniago <gfmaniago@gmail.com>
2015-09-28 23:16:21 -0500

    added job row compoenent

---
+++ scripts/components/JobRowComponent.js
@@ -0,0 +1,20 @@
+var React = require('react');
+
+module.exports = React.createClass({
+	render: function() {
+			<div className="jobTips">
+            <h3 className="tipsHr">Tips for your job posting</h3>
+            <p><strong>Add Keywords</strong>because the majority of candidates search for available positions using keywords, make sure you use all relevant keywords in your posting.
+            </p>
+            <p><strong>Use Familiar Job Titles.</strong>Use specific but familiar job titles in your postings. Make sure the titles are clear and succint.
+            </p>
+            <p><strong>Give Them Details.</strong>The purpose of posting a job is to sparl a candidates interest in the available position. When job postings have detailed descriptions, candidates tend to apply to them more.
+            </p>
+            <p><strong>Expand Your Location</strong>Do not limit your job posting to a restricted area the jobs location. Make sure to include surrounding cities and metropolitan areas in your searches.
+            </p>
+            <p><strong>Discuss Compensation</strong>Even though you may not want to give an exact compensation, give a range. Make sure to point out any bonuses, commissions, or non-monetary compensation, as well.
+            </p>
+        </div>
+	}
+	
+});
